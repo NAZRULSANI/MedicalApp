e6a815c8096f8bf3057d35b16840f1afc698110f
DonPablo <asimjawad2723@gmail.com>
2020-10-11 20:51:21 +0500

    Adding the main activity to the app have the all of the features listed in it.

---
+++ app/src/main/java/com/example/medicalapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.medicalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button emergencyBtn,medicationBtn,faqBtn,firstaidBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        emergencyBtn = findViewById(R.id.emergencyBtn);
+        medicationBtn = findViewById(R.id.firstAidBtn);
+        faqBtn = findViewById(R.id.faqBtn);
+        firstaidBtn = findViewById(R.id.medicationBTn);
+
+        emergencyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone = "911";
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+                startActivity(intent);
+            }
+        });
+
+        firstaidBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,FirstAid.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/medicalapp/SplashActivity.java
+++ app/src/main/java/com/example/medicalapp/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.medicalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 1000);
     }
 }
