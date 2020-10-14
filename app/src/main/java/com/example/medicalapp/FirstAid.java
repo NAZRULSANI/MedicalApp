d17056dcc6b708f9d4ff65e7c4face1be5902e67
DonPablo <asimjawad2723@gmail.com>
2020-10-14 13:44:42 +0500

    adding the cut ativity

--- app/src/main/java/com/example/medicalapp/FirstAid.java
+++ app/src/main/java/com/example/medicalapp/FirstAid.java
@@ -1,8 +1,12 @@
 package com.example.medicalapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstAid extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class FirstAid extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_aid);
+        Button home  = findViewById(R.id.homeBtnFirst);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstAid.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button cut = findViewById(R.id.cutBtn);
+        cut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
