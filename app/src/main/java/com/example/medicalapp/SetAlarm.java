5c2f2735cea775b89c209cfaf93661256dfc86ce
DonPablo <asimjawad2723@gmail.com>
2020-10-14 13:32:17 +0500

    adding the alarm code to the application

--- app/src/main/java/com/example/medicalapp/SetAlarm.java
+++ app/src/main/java/com/example/medicalapp/SetAlarm.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SetAlarm extends AppCompatActivity {
 
@@ -17,6 +18,15 @@ public class SetAlarm extends AppCompatActivity {
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(SetAlarm.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button alarm = findViewById(R.id.setAlarmBtn);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SetAlarm.this, "The alarm has been added", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(SetAlarm.this,MainActivity.class);
                 startActivity(intent);
             }
