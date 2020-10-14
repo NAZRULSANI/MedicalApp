e9ab6cdccdfff9f17e241b820bb4c29a29f8e8c2
DonPablo <asimjawad2723@gmail.com>
2020-10-14 13:31:01 +0500

    Adding the Set alarm to home activity button

--- app/src/main/java/com/example/medicalapp/SetAlarm.java
+++ app/src/main/java/com/example/medicalapp/SetAlarm.java
@@ -2,7 +2,10 @@ package com.example.medicalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SetAlarm extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class SetAlarm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_alarm);
+        Button home = findViewById(R.id.homeBtnSetAlarm);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SetAlarm.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
